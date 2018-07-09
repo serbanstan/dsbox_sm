e8f34d67d8934d22714b11506c88f0a19960615b
Serban Stan <stan@dsbox01.isi.edu>
2018-07-08 18:45:46 -0700

    new files

--- sequential_model.py
+++ sequential_model.py
@@ -8,24 +8,58 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.utils import np_utils
 
-from d3m.primitive_interfaces.base import CallResult   
-import d3m.container as container    
+from d3m.primitive_interfaces.base import CallResult
+from d3m.primitive_interfaces.supervised_learning import SupervisedLearnerPrimitiveBase 
+import d3m.container as container
+import d3m.metadata.hyperparams as hyperparams
+import d3m.metadata.params as params
+
+import typing
 
 Input = container.DataFrame
 Output = container.DataFrame
 
-class sequence_model:
-    def __init__(self, regVal = 0.0001, actHidden = 'tanh', actO = 'sigmoid', maxnormVal = 4, lr = 0.001):
-        self.regVal = regVal
-        self.actHidden = actHidden
-        self.actO = actO
-        self.maxnormVal = maxnormVal
-        self.lr = lr
+class SM_Params(params.Params):
+    model: typing.Union[Sequential, None]
+
+class SM_Hyperparams(hyperparams.Hyperparams):
+    pass
+
+class SequentialModel(SupervisedLearnerPrimitiveBase[Input, Output, SM_Params, SM_Hyperparams]):
+    metadata = PrimitiveMetadata({
+        "schema": "v0",
+        "id": "6cc829ba74786110c636dee4c4ba92bbbd8a8d14",
+        "version": "1.0.0",
+        "name": "Sequential_Model",
+        "description": "Uses Sequential from Keras to do predictions with previously finely tuned hyperparams.",
+        "python_path": "d3m.primitives.dsbox.SequentialModel",
+        "original_python_path": "sequential_model.SequentialModel",
+        "source": {
+            "name": "ISI",
+            "contact": "mailto:sstan@usc.edu",
+            "uris": [ "https://github.com/serbanstan/sequential-model" ]
+        },
+        "installation": [ cfg_.INSTALLATION ],
+        "algorithm_types": [],
+        "primitive_family": "",
+        "hyperparams_to_tune": []
+    })
+
+
+    def __init__(self, *, hyperparams : SM_Hyperparams) -> None:
+        super().__init__(hyperparams = hyperparams)
+
+    def set_training_data(self, *, inputs : Input, outputs: Output) -> None:
+        # initialize the default parameters
+        self.regVal = 0.0001
+        self.actHidden = 'tanh'
+        self.actO = 'sigmoid'
+        self.maxnormVal = 4
+        self.lr = 0.001
         self.validatSplitRate = 0.2
         self.epochs = 30
         self.batchSize = 10
 
-    def set_training_data(self, *, inputs : Input, outputs: Output) -> None:
         # work in DF format
         indeM = len(inputs[0])
         numberOfoutlayerUnit = np.unique(outputs).shape[0]
@@ -60,5 +94,12 @@ class sequence_model:
     
     def produce(self, *, inputs : Input, timeout : float = None, iterations : int = None) -> CallResult[Output]:
         return CallResult(self.model.predict(inputs), True, 0)
+
+    def get_params(self) -> SM_Params:
+        return SM_Params()
+
+    def set_params(self, *, params : SM_Params) -> None:
+        self.model = params["model"]
+        pass
 
     
---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="sequencing_model",
+    version="1.0.0",
+    description="A hyperparameter optimized implementation of a Keras Sequential NN"
+    license="AGPL-3.0",
+    author="Serban Stan/Hyunjun Choi",
+    author_email="sstan@usc.edu",
+    keywords='d3m_primitive',
+    packages=find_packages(),
+    url='https://github.com/serbanstan/sequential_model',
+    download_url='https://github.com/serbanstan/sequential_model',
+    install_requires=[],
+    long_description=long_description,
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ], 
+    entry_points = {
+        'd3m.primitives': [
+            'dsbox.SequentialModel = sequential_model:SequentialModel'
+        ],
+    }
+)
